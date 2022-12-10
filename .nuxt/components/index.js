5b855d34cd0fb7119192c82d1cef3d3d16a72bcc
tilek <moldaliev47@gmail.com>
2022-12-10 17:38:13 +0300

    some changes

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,4 +1,5 @@
 export { default as ListItem } from '../..\\components\\ListItem.vue'
+export { default as MyHeader } from '../..\\components\\MyHeader.vue'
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
